f81f94d62fa784030f5caf11c4d115b530ae6cc8
Matheus <mreinheimer0@gmail.com>
2024-07-13 14:56:23 -0300

    chore: Update confirmTrip route to confirm trip and send confirmation emails

---
+++ src/lib/dayjs.ts
@@ -0,0 +1,9 @@
+import 'dayjs/locale/pt-br'
+
+import dayjs from 'dayjs'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+
+dayjs.locale('pt-br')
+dayjs.extend(localizedFormat)
+
+export { dayjs }
--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,18 +1,14 @@
 /* eslint-disable camelcase */
 import 'dayjs/locale/pt-br'
 
-import dayjs from 'dayjs'
-import localizedFormat from 'dayjs/plugin/localizedFormat'
 import type { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import nodemailer from 'nodemailer'
 import z from 'zod'
 
+import { dayjs } from '../lib/dayjs'
 import { getMailClient } from '../lib/mail'
 import { prisma } from '../lib/prisma'
-
-dayjs.extend(localizedFormat)
-dayjs.locale('pt-br')
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
